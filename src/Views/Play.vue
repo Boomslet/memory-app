35ae4293989c9aad500b6531b270ca034cfbacc1
Mark Boon <boomer_cant_be_stuffed@Hotmail.com>
2018-07-26 17:37:04 +0800

    logic updates, on click styling

--- src/Views/Play.vue
+++ src/Views/Play.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="container">
     <div class="info-bar">
-      <h2>LEVEL: {{ level }}</h2>
-      <button class="btn btn-primary" @click="navigateTo('menu')">BACK</button>
-      <button class="btn btn-primary" @click="generateSequence">GEN</button>
-      <button class="btn btn-primary" @click="playSequence">PLAY</button>
+      <h2>LEVEL {{ level }}</h2>
+      <div class="spaced-div">
+        <button class="button btn btn-primary" :disabled="gameInProgress" @click="beginGame">PLAY</button>
+        <button class="button btn btn-primary" @click="navigateTo('menu')">BACK</button>
+      </div>
     </div>
     <div class="screen">
       <div class="box card" @click="userSelect(i)" v-for="(n, i) in 16" :key="n" :ref="i"></div>
@@ -16,23 +17,31 @@
 export default {
   data() {
     return {
-      sequence: [],
+      count: 0,
       currentUserIndex: 0,
-      level: 1
+      gameInProgress: false,
+      level: 1,
+      sequence: []
     };
   },
   methods: {
     navigateTo(uri) {
       this.$router.push({ name: uri });
     },
+    beginGame() {
+      this.generateSequence();
+      this.playSequence();
+    },
     generateSequence() {
       this.sequence = [];
 
-      for (let i = 0; i < 5; i++) {
+      for (let i = 0; i < this.level + 4; i++) {
         this.sequence.push(Math.floor(Math.random() * 15) + 1);
       }
     },
     playSequence() {
+      this.gameInProgress = true;
+
       let i = 1;
 
       this.sequence.forEach(index => {
@@ -47,17 +56,33 @@ export default {
     },
     userSelect(index) {
       if (index === this.sequence[this.currentUserIndex]) {
-        this.currentUserIndex++;
+        this.count++;
 
-        if (this.currentUserIndex === 5) {
+        this.$refs[index][0].style =
+          "background-color: chartreuse; transition all 0.2s ease-out;";
+        setTimeout(() => {
+          this.$refs[index][0].style = null;
+        }, 200);
+
+        if (++this.currentUserIndex === this.level + 4) {
           this.level++;
           this.currentUserIndex = 0;
           this.generateSequence();
           this.playSequence();
         }
       } else {
-        alert("RIP");
-        return;
+        this.gameInProgress = false;
+
+        this.$refs[index][0].style =
+          "background-color: crimson; transition all 0.2s ease-out;";
+        setTimeout(() => {
+          this.$refs[index][0].style = null;
+        }, 200);
+        
+        alert(`You reach Level ${this.level} - matching ${this.count} tiles!`);
+        this.count = 0;
+        this.currentUserIndex = 0;
+        this.level = 1;
       }
     }
   }
@@ -70,6 +95,11 @@ export default {
   height: 75px;
   margin: 5px;
   width: 75px;
+  transition: all 0.2s ease-out;
+}
+
+.box-clicked {
+  background-color: #8a2be2;
 }
 
 .box-selected {
@@ -81,6 +111,12 @@ export default {
 
 .box:hover {
   background-color: #007bff;
+}
+
+button {
+  font-size: 20px;
+  margin: 20px;
+  width: 200px;
 }
 
 .container {
@@ -99,6 +135,7 @@ h2 {
   flex-direction: row;
   height: 64px;
   justify-content: space-between;
+  margin-bottom: 32px;
   width: 100%;
 }
 
@@ -110,6 +147,13 @@ h2 {
   height: 400px;
   margin: auto;
   width: 400px;
+}
+
+.spaced-div {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: calc(auto + 100px);
 }
 </style>
 
